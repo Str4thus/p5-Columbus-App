b4fd9cb3ea3f0a0ce1a4c23ec5795165a32d589f
Str4thus <trashytrashmail42@gmail.com>
2020-03-15 23:01:33 +0100

    refactored module controller + added unit testing

--- src/app/services/module-controller/module-controller.service.spec.ts
+++ src/app/services/module-controller/module-controller.service.spec.ts
@@ -1,15 +1,13 @@
-import { TestBed } from '@angular/core/testing';
-
 import { ModuleControllerService } from './module-controller.service';
 import { IStateData } from 'src/columbus/data-models/modules/concrete-states/IStateData';
 import { ModuleDataService } from '../module-data/module-data.service';
 import { ColumbusModule } from 'src/columbus/data-models/modules/ColumbusModule';
 import { ColumbusModuleType } from 'src/columbus/util/Enums';
 
-/*
+
 class MockControllerService extends ModuleControllerService<IStateData> {
   constructor(mockModuleDataService: ModuleDataService) {
-    super(mockModuleDataService, null);
+    super(mockModuleDataService, ColumbusModuleType.TEST);
   }
 
   onStateChange(newState: IStateData) { }
@@ -17,20 +15,11 @@ class MockControllerService extends ModuleControllerService<IStateData> {
 
 describe('AbstractModuleControllerService', () => {
   let mockService: ModuleControllerService<IStateData>;
-  let currentModuleState: ColumbusModule;
   let mockModuleDataService;
 
-  const startState: IStateData = {
-    vrot: 0,
-    hrot: 0
-  }
-
   beforeEach(() => {
-    // Data
-    currentModuleState = new ColumbusModule(ColumbusModuleType.TEST, startState);
-
-    // Mocks
-    mockModuleDataService = createMockModuleDataServiceForControllers();
+    // Mock
+    mockModuleDataService = jasmine.createSpyObj("moduleDataSerivce", ["addModule", "removeModule", "updateState", "getModuleState", "isModulePresent", "subscribeToModule"]);
 
     // Service
     mockService = new MockControllerService(mockModuleDataService);
@@ -40,33 +29,105 @@ describe('AbstractModuleControllerService', () => {
     expect(mockService).toBeTruthy();
   });
 
-  describe("General", () => {
-    it('should have correct controlling module type', () => {
-      expect(mockService.controllingModuleType).toEqual(null);
+  describe("Data on creation", () => {
+    it("should retrieve state data from ModuleDataService on creation if module is already connected", () => {
+      mockModuleDataService.isModulePresent.and.callFake(() => { return true });
+      mockModuleDataService.getModuleState.and.callFake(() => { return { "default": true } });
+      mockService = new MockControllerService(mockModuleDataService);
+
+      expect(mockService.canOperate.value).toBeTruthy();
+      expect(mockModuleDataService.getModuleState).toHaveBeenCalled();
+      expect(mockService.moduleStateDataCopy.value).toEqual({ "default": true });
+    });
+
+    it("should not get any data on creation if the module is not connected", () => {
+      mockModuleDataService.isModulePresent.and.callFake(() => { return false });
+      mockService = new MockControllerService(mockModuleDataService);
+
+      expect(mockService.canOperate.value).toBeFalsy();
+      expect(mockModuleDataService.getModuleState).not.toHaveBeenCalled();
+      expect(mockService.moduleStateDataCopy.value).toEqual({});
     });
   });
 
   describe("Updating", () => {
-    it('should correctly invoke updateModuleState of ModuleDataService', () => {
-      let newStateData: IStateData = {};
-
-      mockService.setStateData(newStateData);
-
-      expect(mockModuleDataService.updateModuleState).toHaveBeenCalledWith(null, newStateData);
+    it("should update the state data copy correctly", () => {
+      expect(mockService.moduleStateDataCopy.value).toEqual({});
+      mockService._updateStateDataCopy({ "default": true });
+      expect(mockService.moduleStateDataCopy.value).toEqual({ "default": true });
     });
 
-    it('should correctly invoke onStateChange when the state changed', () => {
-      console.log(mockService);
-      mockModuleDataService.foo(mockService);
+    it("should update the state data copy on change", () => {
+      let registeredCallback: ((updatedModule: ColumbusModule) => void) = null;
+      let callbackSpy = jasmine.createSpy("registeredCallback", registeredCallback);
+      let testModule: ColumbusModule = new ColumbusModule(ColumbusModuleType.TEST, {"test": true});
 
-      spyOn(mockService, "onStateChange");
-      let newStateData: IStateData = {"test": true}
+      // Mock setup
+      mockModuleDataService.subscribeToModule.and.callFake((type, callback: (updatedModule) => void) => {
+        registeredCallback = callback;
+      });
+      mockModuleDataService.addModule.and.callFake((module: ColumbusModule) => {
+        callbackSpy(module);
+        mockService.moduleStateDataCopy.next(module.getCurrentState());
+      })
+      mockModuleDataService.updateState.and.callFake((type, newStateData) => {
+        callbackSpy(new ColumbusModule(type, newStateData));
+        mockService.moduleStateDataCopy.next(newStateData);
+      })
+
+      // Create mock service 
+      mockService = new MockControllerService(mockModuleDataService);
       
-      mockService.setStateData(newStateData);
+      // Trigger update callback with testmodule and check if the state changes accordingly
+      mockModuleDataService.addModule(testModule);
+      expect(callbackSpy).toHaveBeenCalledWith(testModule);
+      expect(mockService.moduleStateDataCopy.value).toEqual(testModule.getCurrentState());
 
-      expect(mockModuleDataService.subscribeToModuleChanges).toHaveBeenCalled();
-      expect(mockModuleDataService.notifyObservers).toHaveBeenCalledWith(newStateData);
+      // Update the module state to trigger another update callback and create expected new module object
+      mockModuleDataService.updateState(ColumbusModuleType.TEST, { "updated": true });
+      let expectedModule = new ColumbusModule(ColumbusModuleType.TEST, { "updated": true });
+      
+      // Trigger update callback with the expected module and check if the state matches
+      expect(callbackSpy).toHaveBeenCalledWith(expectedModule);
+      expect(mockService.moduleStateDataCopy.value).toEqual(expectedModule.getCurrentState());
+
+      expect(mockModuleDataService.subscribeToModule).toHaveBeenCalled();
+      expect(mockModuleDataService.addModule).toHaveBeenCalled();
+      expect(mockModuleDataService.updateState).toHaveBeenCalled();
+    });
+
+    it("should become unable to operate if the module disconnects", () => {
+      let testModule = new ColumbusModule(ColumbusModuleType.TEST);
+      expect(mockService.canOperate.value).toBeFalsy();
+
+      mockService._subscribeCallback(testModule);
+      expect(mockService.canOperate.value).toBeTruthy();
+
+      mockService._subscribeCallback(null);
+      expect(mockService.canOperate.value).toBeFalsy();
+    });
+  });
+
+  describe("Manipulating module", () => {
+    it("should be able to apply changes to the module", () => {
+      mockService.canOperate.next(true);
+      expect(mockService.moduleStateDataCopy.value).toEqual({});
+
+      mockService.moduleStateDataCopy.value["hi"] = 0;
+      mockService.applyChanges();
+
+      expect(mockService.moduleStateDataCopy.value).toEqual({"hi": 0});
+      expect(mockModuleDataService.updateState).toHaveBeenCalled();
+    });
+
+    it("is not able to apply changes if the module is not connected", () => {
+      mockService.canOperate.next(true);
+      mockService.applyChanges();
+      expect(mockModuleDataService.updateState).toHaveBeenCalledTimes(1);
+
+      mockService.canOperate.next(false);
+      mockService.applyChanges();
+      expect(mockModuleDataService.updateState).toHaveBeenCalledTimes(1);
     });
   });
 });
-*/
--- src/app/services/module-controller/module-controller.service.ts
+++ src/app/services/module-controller/module-controller.service.ts
@@ -1,13 +1,47 @@
 import { Injectable } from '@angular/core';
-import { ModuleDataService } from '../module-data/module-data.service';
 import { IStateData } from 'src/columbus/data-models/modules/concrete-states/IStateData';
+import { ModuleDataService } from '../module-data/module-data.service';
 import { ColumbusModuleType } from 'src/columbus/util/Enums';
-import { Utils } from 'src/columbus/util/Utils';
 import { BehaviorSubject } from 'rxjs';
+import { ColumbusModule } from 'src/columbus/data-models/modules/ColumbusModule';
+import { Utils } from 'src/columbus/util/Utils';
 
 @Injectable({
   providedIn: 'root'
 })
 export abstract class ModuleControllerService<T extends IStateData> {
-  
+  moduleStateDataCopy: BehaviorSubject<T> = new BehaviorSubject({} as T);
+  canOperate: BehaviorSubject<boolean> = new BehaviorSubject(false);
+
+  constructor(protected moduleDataSerivce: ModuleDataService, public responsibleForModuleType: ColumbusModuleType) {
+    if (this.moduleDataSerivce.isModulePresent(responsibleForModuleType)) {
+      this._updateStateDataCopy(this.moduleDataSerivce.getModuleState(responsibleForModuleType));
+
+      this.canOperate.next(true);
+    }
+
+    this.moduleDataSerivce.subscribeToModule(responsibleForModuleType, this._subscribeCallback);
+  }
+
+  _subscribeCallback(updatedModule: ColumbusModule) {
+    if (updatedModule) {
+      this._updateStateDataCopy(updatedModule.getCurrentState());
+
+      if (!this.canOperate.value) {
+        this.canOperate.next(true);
+      }
+    } else {
+      if (this.canOperate.value) 
+        this.canOperate.next(false);
+    }
+  }
+
+  _updateStateDataCopy(newStateData: IStateData) {
+    this.moduleStateDataCopy.next(Utils.deepClone(newStateData));
+  }
+
+  applyChanges() {
+    if (this.canOperate.value)
+      this.moduleDataSerivce.updateState(this.responsibleForModuleType, this.moduleStateDataCopy.value);
+  }
 }
