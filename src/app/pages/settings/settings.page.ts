970c657ba4d27ec93f794e0673df30b6cf5e6fa5
a <a>
2020-05-08 22:39:32 +0200

    Added Socket adress Setting

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage implements OnInit {
-
+  currentUrl: string = '';
   constructor() { }
 
   ngOnInit() {
   }
-
+  setSocketAdress(){
+    //TODO: Set Configurations to new Socket adress 
+    //this.currentUrl is the Socket Host
+    console.log(this.currentUrl);
+  }
 }
